a634c069a769c6991de629e1c606e8b78ca98425
olaven <olav@sundfoer.com>
2019-09-23 11:15:23 +0200

    using own heoku module - first pass

--- heroku.tf
+++ heroku.tf
@@ -1,50 +1,8 @@
-provider "heroku" {}
-
-
-## Creating apps
-
-resource "heroku_app" "test" {
-  name = "trees-ci"
-  region = "eu"
-  buildpacks = [
-    "heroku/node",
-    "heroku/java"
-  ]
-}
-
-resource "heroku_app" "staging" {
-  name = "trees-staging"
-  region = "eu"
+module "olaven-heroku" {
+  source = "github.com/olaven/heroku-terraform-module"
+  name = "trees"
   buildpacks = [
     "heroku/nodejs",
     "heroku/java"
   ]
-}
-
-resource "heroku_app" "production" {
-  name = "trees-production"
-  region = "eu"
-  buildpacks = [
-    "heroku/node",
-    "heroku/java"
-  ]
-}
-
-
-## Creating pipelines
-
-resource "heroku_pipeline" "deploy" {
-  name = "trees-deploy"
-}
-
-resource "heroku_pipeline_coupling" "staging" {
-  app = heroku_app.staging.name
-  pipeline = heroku_pipeline.deploy.id
-  stage = "staging"
-}
-
-resource "heroku_pipeline_coupling" "production" {
-  app = heroku_app.production.name
-  pipeline = heroku_pipeline.deploy.id
-  stage = "production"
 }
