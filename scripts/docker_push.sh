2b3e7668acc955389a041b4364d4dd9160ac1f94
olaven <olav@sundfoer.com>
2019-09-30 11:26:45 +0200

    fixed image name in script

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build . --tag trees-image
-docker tag trees-image olaven/trees-image:latest
+docker build . --tag trees
+docker tag trees olaven/trees:latest
 docker push olaven/trees
