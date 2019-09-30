ded7e4503febc530ca912ac8d57c85b87baf3bce
olaven <olav@sundfoer.com>
2019-09-30 11:05:12 +0200

    tweaked script

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build . --tag trees-image
-docker tag trees-image <dockerhub_username>/<image_name>:latest
-docker push olaven/trees
+docker tag trees-image $(DOCKER_USERNAME)/trees-image:latest
+docker push $(DOCKER_USERNAME)/trees
