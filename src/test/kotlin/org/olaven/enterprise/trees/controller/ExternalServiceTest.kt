44f080d8e7f3acf59ba95ca3debc0fa9028e2d9e
olaven <olav@sundfoer.com>
2019-10-02 21:56:42 +0200

    used mocking to test external API

---
+++ src/test/kotlin/org/olaven/enterprise/trees/controller/ExternalServiceTest.kt
@@ -0,0 +1,73 @@
+package org.olaven.enterprise.trees.controller
+
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.common.ConsoleNotifier
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
+import io.restassured.RestAssured.given
+import io.restassured.http.ContentType
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+
+internal class ExternalServiceTest: ControllerTestBase() {
+
+
+    companion object {
+
+        private var wiremockServer: WireMockServer = WireMockServer(WireMockConfiguration.wireMockConfig().port(8099).notifier(ConsoleNotifier(true)))
+
+        @BeforeAll
+        @JvmStatic
+        fun `init wiremock`() {
+
+            wiremockServer.start()
+        }
+
+        @AfterAll
+        @JvmStatic
+        fun tearDown() {
+            wiremockServer.stop()
+        }
+
+    }
+
+    @Test
+    fun `getting external gives 200`() {
+
+        stubresponse()
+        given()
+                .accept(ContentType.JSON)
+                .get("/external")
+                .then()
+                .statusCode(200)
+    }
+
+
+    private fun stubresponse() {
+
+        val json = """
+        [
+            { "userId": 1, "id": 1, "title": "Title 1", "completed": false },
+            { "userId": 1, "id": 2, "title": "Title 2", "completed": true },
+            { "userId": 2, "id": 3, "title": "Title 3", "completed": true },
+            { "userId": 3, "id": 4, "title": "Title 4", "completed": false },
+            { "userId": 2, "id": 5, "title": "Title 5", "completed": true }
+        ]
+        """
+
+        wiremockServer.stubFor(//prepare a stubbed response for the given request
+                WireMock.get(//define the GET request to mock
+                        /*
+                           recall regular expressions:
+                           "." =  any character
+                           "*" = zero or more times
+                        */
+                        WireMock.urlMatching("/api/external"))
+                        // define the mocked response of the GET
+                        .willReturn(WireMock.aResponse()
+                                .withHeader("Content-Type", "application/json; charset=utf-8")
+                                .withHeader("Content-Length", "" + json.toByteArray(charset("utf-8")).size)
+                                .withBody(json)))
+    }
+}
