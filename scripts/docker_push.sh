2665b1c33fb53c1b52a47d1dc356579e75bde724
olaven <olav@sundfoer.com>
2019-10-16 16:51:04 +0200

    using docker compose to automatically push every microservice

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build . --tag org.olaven.enterprise.trees
-docker tag org.olaven.enterprise.trees olaven/org.olaven.enterprise.trees:latest
-docker push olaven/org.olaven.enterprise.trees
+docker-compose push
