425fcd1d4d894c844f1379edbcadd0498b3f55e2
olaven <olav@sundfoer.com>
2019-10-16 19:35:58 +0200

    first pass at adding service regisery (eureka) and gateway

---
+++ gateway/src/main/kotlin/org/olaven/enterprise/trees/gateway/GatewayApplication.kt
@@ -0,0 +1,14 @@
+package org.olaven.enterprise.trees.gateway
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+
+@EnableDiscoveryClient
+@SpringBootApplication
+class GatewayApplication
+
+
+fun main(args: Array<String>) {
+    SpringApplication.run(GatewayApplication::class.java, *args)
+}
