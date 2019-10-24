6fe09f99acd6e90aa07cdb81482f05281ac1cebf
olaven <olav@sundfoer.com>
2019-10-24 22:45:46 +0200

    gateway shows up in eureka

--- gateway/src/main/kotlin/org/olaven/enterprise/trees/gateway/GatewayApplication.kt
+++ gateway/src/main/kotlin/org/olaven/enterprise/trees/gateway/GatewayApplication.kt
@@ -3,12 +3,12 @@ package org.olaven.enterprise.trees.gateway
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@EnableEurekaClient
-class GatewayApplication
+class GatewayApplication {
+}
 
 
 fun main(args: Array<String>) {
