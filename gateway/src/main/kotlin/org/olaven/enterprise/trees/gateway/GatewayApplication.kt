a8115d50a4b7ff972ba3b6e215249d7e046ac72e
olaven <olav@sundfoer.com>
2019-10-31 11:44:03 +0100

    changed name of jar files + LOTS of debugging of gateway

--- gateway/src/main/kotlin/org/olaven/enterprise/trees/gateway/GatewayApplication.kt
+++ gateway/src/main/kotlin/org/olaven/enterprise/trees/gateway/GatewayApplication.kt
@@ -4,12 +4,9 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
-
 @EnableDiscoveryClient
 @SpringBootApplication
-class GatewayApplication {
-}
-
+class GatewayApplication
 
 fun main(args: Array<String>) {
     SpringApplication.run(GatewayApplication::class.java, *args)
