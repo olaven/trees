97fc940e3e507846977964ce9bbda28f01e86eea
olaven <olav@sundfoer.com>
2019-11-01 00:18:47 +0100

    working on getting integration tests to work

---
+++ integration_tests/src/test/kotlin/GatewayIntegrationDockerTestBase.kt
@@ -0,0 +1,84 @@
+package org.olaven.enterprise.trees.integration_tests
+
+import io.restassured.RestAssured
+import io.restassured.RestAssured.given
+import org.awaitility.Awaitility.await
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.jupiter.api.BeforeAll
+import org.testcontainers.containers.DockerComposeContainer
+import org.testcontainers.containers.wait.strategy.Wait
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import java.io.File
+import java.time.Duration
+import java.util.concurrent.TimeUnit
+
+@Testcontainers
+abstract class GatewayIntegrationDockerTestBase {
+
+    companion object {
+
+        init {
+            RestAssured.enableLoggingOfRequestAndResponseIfValidationFails()
+            RestAssured.port = 80
+        }
+
+        class KDockerComposeContainer(id: String, path: File) : DockerComposeContainer<KDockerComposeContainer>(id, path)
+
+        @Container
+        @JvmField
+        val env = KDockerComposeContainer("gateway", File("../docker-compose.yml"))
+                /*
+                    In the docker-compose file, I did not expose the port for Eureka.
+                    Nor I made a route for it in the gateway.
+                    However, I need it in these tests, as need to query it to wait for it
+                    be fully initialized.
+                    So, I expose it here.
+                 */
+                .withExposedService("registry", 8761,
+                        /*
+                            By default, Testcontainer will wait up to 60 seconds for the exposed
+                            port to be accessible.
+                            This is too low value on free CIs like Travis when there are many
+                            Docker images running
+                         */
+                        Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(240)))
+                .withLocalCompose(true)
+
+
+        @BeforeAll
+        @JvmStatic
+        fun waitForServer() {
+
+            /*
+                Need to wait for a bit, because it can take up to 2 minutes before
+                Eureka is fully initialized and all changes are propagated to all
+                of its clients. See:
+                https://github.com/Netflix/eureka/wiki/Understanding-eureka-client-server-communication
+
+                Note: here I am using the Awaitility library to do such waits
+             */
+
+            await().atMost(180, TimeUnit.SECONDS)
+                    .ignoreExceptions()
+                    .until {
+
+                        given().get("/index.html").then().statusCode(200)
+
+                        given().accept("application/json;charset=UTF-8")
+                                .get("/api/locations")
+                                .then().statusCode(200)
+
+                        given().baseUri("http://${env.getServiceHost("registry", 8761)}")
+                                .port(env.getServicePort("registry", 8761))
+                                .get("/eureka/apps")
+                                .then()
+                                .body("applications.application.instance.size()", equalTo(4))
+
+                        true
+                    }
+        }
+
+    }
+
+}
---
+++ integration_tests/src/test/kotlin/GatewayRestIT.kt
@@ -0,0 +1,88 @@
+package org.olaven.enterprise.trees.integration_tests
+
+import io.restassured.RestAssured
+import io.restassured.RestAssured.given
+import io.restassured.http.ContentType
+import org.awaitility.Awaitility
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.testcontainers.junit.jupiter.Testcontainers
+import java.util.concurrent.TimeUnit
+
+/**
+ * Created by arcuri82 on 04-Oct-19.
+ */
+@Testcontainers
+class GatewayRestIT : GatewayIntegrationDockerTestBase() {
+
+    companion object {
+        init {
+            RestAssured.enableLoggingOfRequestAndResponseIfValidationFails()
+        }
+    }
+
+    @BeforeEach
+    fun clean(){
+        given().delete("/service/messages")
+                .then()
+                .statusCode(204)
+
+        given().get("/service/messages")
+                .then()
+                .statusCode(200)
+                .body("size()", equalTo(0))
+    }
+
+    private fun sendMsg(msg: String){
+
+        given().contentType(ContentType.TEXT)
+                .body(msg)
+                .post("/service/messages")
+                .then()
+                .statusCode(201)
+    }
+
+    @Test
+    fun testIntegration() {
+
+        given().get("/service/messages")
+                .then()
+                .statusCode(200)
+                .body("size()", equalTo(0))
+
+
+        sendMsg("Hail Zuul!!!")
+        sendMsg("Just kidding")
+
+        given().get("/service/messages")
+                .then()
+                .statusCode(200)
+                .body("size()", equalTo(2))
+    }
+
+    @Test
+    fun testLoadBalance() {
+
+
+        Awaitility.await().atMost(120, TimeUnit.SECONDS)
+                .ignoreExceptions()
+                .until{
+                    (0..3).forEach { sendMsg("foo") }
+
+                    val messages = given().get("/service/messages")
+                            .then()
+                            .statusCode(200)
+                            .extract().body().jsonPath().getList("system", String::class.java)
+
+                    assertEquals(3, messages.toSet().size)
+                    assertTrue(messages.contains("A"))
+                    assertTrue(messages.contains("B"))
+                    assertTrue(messages.contains("C"))
+
+                    true
+                }
+    }
+}
